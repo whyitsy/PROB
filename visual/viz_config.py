65fe6625f19fd7a4f3d96f6d8dfd751733b0822a
kakoyo <50785908+whyitsy@users.noreply.github.com>
2026-04-19 04:29:53 +0800

    Add embedding visualization defaults

--- visual/viz_config.py
+++ visual/viz_config.py
@@ -29,6 +29,29 @@ DEFAULT_VIZ_CFG = {
     'save_error_summary_csv': True,
     'save_query_distribution_plots': True,
     'save_feature_embedding_plots': True,
+    # Display filtering
+    'display_known_score_thresh': 0.25,
+    'display_unknown_score_thresh': 0.25,
+    'display_apply_geometry_filter': False,
+    'display_min_area_ratio': 0.0001,
+    'display_min_side_ratio': 0.01,
+    'display_max_aspect_ratio': 20.0,
+    'display_nms_iou': 0.60,
+    # Embedding controls
+    'embedding_dims': [2, 3],
+    'embedding_methods': ['pca', 'tsne'],
+    'embedding_min_points_2d': 8,
+    'embedding_min_points_3d': 12,
+    'embedding_generic_max_points_2d': 2000,
+    'embedding_generic_max_points_3d': 1500,
+    'embedding_tsne_max_points_2d': 800,
+    'embedding_tsne_max_points_3d': 600,
+    'embedding_umap_max_points_2d': 1200,
+    'embedding_umap_max_points_3d': 900,
+    'embedding_random_state': 42,
+    'embedding_tsne_perplexity_cap': 30,
+    'embedding_umap_n_neighbors': 20,
+    'embedding_umap_min_dist': 0.15,
 }
 
 
