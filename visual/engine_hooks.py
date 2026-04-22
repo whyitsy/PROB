8effd0cffc6e299d35aca3cecfcdbbfae4c53e4b
kakoyo <50785908+whyitsy@users.noreply.github.com>
2026-04-22 16:21:02 +0800

    Align engine hooks with minimal tensorboard-only logging

--- visual/engine_hooks.py
+++ visual/engine_hooks.py
@@ -1,4 +1,3 @@
-import util.misc as utils
 from util.visual.training import write_train_step_artifacts
 
 
@@ -18,35 +17,31 @@ def log_train_step_artifacts(
     reduced_model_stat_dict=None,
     args=None,
 ):
-    if viz_ctx is None or not viz_ctx.should_write_artifacts:
+    del outputs, targets, criterion, args, epoch, local_step
+    if viz_ctx is None or viz_ctx.tb_writer is None:
         return
     write_train_step_artifacts(
         tb_writer=viz_ctx.tb_writer,
-        step_jsonl_path=viz_ctx.train_step_metrics_path,
         global_step=global_step,
-        epoch=epoch,
-        local_step=local_step,
         optimizer=optimizer,
         grad_total_norm=grad_total_norm,
-        outputs=outputs,
-        targets=targets,
-        criterion=criterion,
         total_loss=total_loss,
         reduced_loss_dict=reduced_loss_dict,
         reduced_weighted_loss_dict=reduced_weighted_loss_dict,
         reduced_model_stat_dict=reduced_model_stat_dict,
-        viz_cfg=viz_ctx.viz_cfg,
-        args=args,
     )
 
 
 def init_eval_visual_state(viz_ctx):
+    del viz_ctx
     return None
 
 
 def collect_eval_visuals(viz_ctx, visual_state, samples, targets, visual_results, outputs, criterion, args, epoch=0):
+    del viz_ctx, visual_state, samples, targets, visual_results, outputs, criterion, args, epoch
     return
 
 
 def finalize_eval_visuals(viz_ctx, visual_state, epoch=0):
+    del viz_ctx, visual_state, epoch
     return {}
