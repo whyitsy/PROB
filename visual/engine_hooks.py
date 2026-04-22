915359651c92e9f6a2ff4e5b44106c1750aecdd3
kakoyo <50785908+whyitsy@users.noreply.github.com>
2026-04-22 16:12:54 +0800

    Simplify engine hooks to only log training steps and remove eval visualization

--- visual/engine_hooks.py
+++ visual/engine_hooks.py
@@ -1,11 +1,5 @@
 import util.misc as utils
-from util.visual.evaluation import collect_eval_visual_stats_aligned, compute_branch_correlation_metrics
 from util.visual.training import write_train_step_artifacts
-from visual.eval_visualizer import (
-    finalize_eval_visualizations,
-    init_eval_visual_state as _init_eval_visual_state,
-    save_eval_qualitative_cases,
-)
 
 
 def log_train_step_artifacts(
@@ -47,46 +41,12 @@ def log_train_step_artifacts(
 
 
 def init_eval_visual_state(viz_ctx):
-    if viz_ctx is None or not viz_ctx.visualization_enabled or not utils.is_main_process():
-        return None
-    return _init_eval_visual_state(viz_ctx.viz_cfg)
+    return None
 
 
 def collect_eval_visuals(viz_ctx, visual_state, samples, targets, visual_results, outputs, criterion, args, epoch=0):
-    if viz_ctx is None or visual_state is None:
-        return
-    collect_eval_visual_stats_aligned(visual_state, outputs, targets, criterion, args)
-    visual_output_dir = viz_ctx.eval_visualization_dir(epoch)
-    save_eval_qualitative_cases(
-        visual_state,
-        samples,
-        targets,
-        visual_results,
-        outputs,
-        criterion,
-        args,
-        str(visual_output_dir),
-        viz_ctx.viz_cfg,
-        tb_writer=viz_ctx.tb_writer,
-        global_step=epoch,
-        epoch=epoch,
-    )
+    return
 
 
 def finalize_eval_visuals(viz_ctx, visual_state, epoch=0):
-    if viz_ctx is None or visual_state is None:
-        return {}
-
-    extra_metrics = {}
-    for key, value in compute_branch_correlation_metrics(visual_state).items():
-        if value is not None:
-            extra_metrics[key] = value
-
-    finalize_eval_visualizations(
-        visual_state,
-        str(viz_ctx.output_dir),
-        epoch,
-        viz_ctx.viz_cfg,
-        tb_writer=viz_ctx.tb_writer,
-    )
-    return extra_metrics
+    return {}
